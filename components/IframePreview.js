41d901247fc4f3a6a95aef053a3a02782ad67e2d
Sotatek-ChienVu <chien.vu@sotatek.com>
2022-03-24 15:27:10 +0700

    feature: fixbug iframe

--- components/IframePreview.js
+++ components/IframePreview.js
@@ -3,18 +3,24 @@ import { Liquid } from "liquidjs";
 import templateFaq from "../liquid_templates/faq-template.liquid";
 import templateFaqSetting from "../liquid_templates/faq-page-template.liquid";
 
-export default function IframePreview({ formik, shop, accessToken, faqGroup, isFaq }) {
+export default function IframePreview({
+  formik,
+  shop,
+  accessToken,
+  faqGroup,
+  isFaq,
+}) {
   const iframe = useRef();
   const engine = new Liquid();
   // const app = NODE_ENV === "development" ? null : null;
   const [faqGroups, setFaqGroups] = useState([]);
 
   const renderGroupSelected = useCallback(async (group, faqGroups) => {
-    const newGroup = group?.map(item => {
-      return faqGroups?.find(groups => groups?.id === item)
-    })
+    const newGroup = group?.map((item) => {
+      return faqGroups?.find((groups) => groups?.id === item);
+    });
     setFaqGroups(newGroup);
-  },[]);
+  }, []);
 
   useEffect(() => {
     if (formik?.values?.groups?.length > 0) {
@@ -24,12 +30,29 @@ export default function IframePreview({ formik, shop, accessToken, faqGroup, isF
 
   useEffect(() => {
     if (faqGroups) {
+      const {
+        faqBehavior,
+        faqNameTag,
+        faqIconPosition,
+        faqExtras,
+        faqIconSelect,
+        faqStyleID,
+      } = formik.values;
+
       render({
-        // config: formik.values,
+        config: {
+          ...formik.values,
+          faqStyleID: faqStyleID[0],
+          faqBehavior: faqBehavior[0],
+          faqNameTag: faqNameTag[0],
+          faqIconPosition: faqIconPosition[0],
+          faqExtras: faqExtras[0],
+          faqIconSelect: faqIconSelect[0],
+        },
         groups: faqGroups,
       });
     }
-  }, [faqGroups]);
+  }, [faqGroups, formik.values]);
 
   const render = async (data) => {
     const document = iframe.current.contentDocument;
@@ -51,9 +74,14 @@ export default function IframePreview({ formik, shop, accessToken, faqGroup, isF
     let style3 = document.createElement("link");
     style3.rel = "stylesheet";
     style3.type = "text/css";
-    style3.href = `${HOST}/${isFaq ? 'faq-preview.css':'faq-page-preview.css'}`;
+    style3.href = `${HOST}/${
+      isFaq ? "faq-preview.css" : "faq-page-preview.css"
+    }`;
     head.appendChild(style3);
-    let html = await engine.parseAndRender(isFaq ? templateFaq : templateFaqSetting, data);
+    let html = await engine.parseAndRender(
+      isFaq ? templateFaq : templateFaqSetting,
+      data
+    );
     document.body.innerHTML = `
       <body>${html}</body>
     `;
@@ -71,7 +99,7 @@ export default function IframePreview({ formik, shop, accessToken, faqGroup, isF
       "https://cdnjs.cloudflare.com/ajax/libs/masonry/4.2.2/masonry.pkgd.min.js";
     document.body.appendChild(masonry);
     let script = document.createElement("script");
-    script.src = `${HOST}/${isFaq ? 'faq-preview.js' : 'faq-page-preview.js'}`;
+    script.src = `${HOST}/${isFaq ? "faq-preview.js" : "faq-page-preview.js"}`;
     document.body.appendChild(script);
   };
 
